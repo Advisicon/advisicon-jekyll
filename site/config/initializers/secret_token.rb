356e7ed749fa6ed62a6638dde859be3a1be7f195
changecase <jeff.ja@gmail.com>
2014-01-13 19:34:09 -0800

    create new RoR app

---
+++ site/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Site::Application.config.secret_key_base = '6a6cbeace70c3246ab298195d11291a110638872bd01bbcdc76fde444ee313b4ba21d62941e54f5991e7136192f19cf84c4c19031f1bed1f722e91529e097919'
